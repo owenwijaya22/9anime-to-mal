18bcf7937739d3ff5e17604bd81c407d7bbb61bc
Owen Valentinus <owenwijaya89@gmail.com>
2022-04-13 15:09:19 +0700

    Added notes and declared file paths as variables for the sake of readability in refreh_token.py

--- src/refresh_token.py
+++ src/refresh_token.py
@@ -2,8 +2,8 @@ import requests
 import json
 from get_access_token import client_id, client_secret
 
-def get_refresh_token():
-    with open('./data/access_token.json', 'r+') as f:
+def get_refresh_token(access_token_file_path):
+    with open(access_token_file_path, 'r+') as f:
         refresh_token = json.load(f)['refresh_token']
     return refresh_token
 def get_fresh_access_token(refresh_token):
@@ -19,15 +19,15 @@ def get_fresh_access_token(refresh_token):
     fresh_access_token = r.json()
     return fresh_access_token
 
-def save_fresh_access_token(fresh_access_token, file_path):
-    with open(file_path, 'w') as f:
+def save_fresh_access_token(fresh_access_token, access_token_file_path):
+    with open(access_token_file_path, 'w') as f:
         json.dump(fresh_access_token, f, indent=4)
 
 def main():
-    file_path = './data/access_token.json'
-    refresh_token = get_refresh_token(file_path)
+    access_token_file_path = './data/access_token.json'
+    refresh_token = get_refresh_token(access_token_file_path)
     fresh_access_token = get_fresh_access_token(refresh_token)
-    save_fresh_access_token(fresh_access_token, file_path)
+    save_fresh_access_token(fresh_access_token, access_token_file_path)
     
 if __name__ == '__main__':
     main()
