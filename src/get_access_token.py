d6829dce11deb02a606597ced51dbb105b9149e7
Owen Valentinus <owenwijaya89@gmail.com>
2022-04-13 15:12:57 +0700

    addded notes to get_access_token.py

--- src/get_access_token.py
+++ src/get_access_token.py
@@ -2,26 +2,32 @@ import requests
 import secrets
 import json
 
+#declared client_id and client_secret made on https://myanimelist.net/apiconfig
 client_id = '963e88e06d2cc42e822fecaae6e86c22'
 client_secret = 'd506643fc1b782b3cb70c5d5168e78aecbe34f0d83669af953000f86ce6899fa'
 
 
+#get code verifier/challenge as a temporary password
 def get_new_code_verifier():
     code_verifier = secrets.token_urlsafe(100)
     return code_verifier[:128]
 
 
+#build the url for mal authentication
 def generate_url(client_id, code_challenge):
     url = f'https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id={client_id}&state=YOUR_STATE&code_challenge={code_challenge}&code_challenge_method=plain'
     print(url)
 
 
+#get the authorization code made after the user authenticates mal
 def generate_code():
-    authorization_code = input('Paste the authorisation code: ').strip()
+    authorization_code = input('Paste the authorization code: ').strip()
     return authorization_code
 
 
-def generate_access_token(client_id, client_secret, authorization_code, code_verifier):
+#get the access token to myanimelist's api
+def generate_access_token(client_id, client_secret, authorization_code,
+                          code_verifier):
     url = 'https://myanimelist.net/v1/oauth2/token'
     payload = {
         'client_id': client_id,
@@ -46,4 +52,5 @@ if __name__ == '__main__':
     code_verifier = code_challenge = get_new_code_verifier()
     generate_url(client_id, code_challenge)
     authorization_code = generate_code()
-    generate_access_token(client_id, client_secret, authorization_code, code_verifier)
+    generate_access_token(client_id, client_secret, authorization_code,
+                          code_verifier)
--- src/refresh_token.py
+++ src/refresh_token.py
@@ -2,10 +2,13 @@ import requests
 import json
 from get_access_token import client_id, client_secret
 
+
 def get_refresh_token(access_token_file_path):
     with open(access_token_file_path, 'r+') as f:
         refresh_token = json.load(f)['refresh_token']
     return refresh_token
+
+
 def get_fresh_access_token(refresh_token):
     refresh_token_url = 'https://myanimelist.net/v1/oauth2/token'
 
@@ -19,15 +22,18 @@ def get_fresh_access_token(refresh_token):
     fresh_access_token = r.json()
     return fresh_access_token
 
+
 def save_fresh_access_token(fresh_access_token, access_token_file_path):
     with open(access_token_file_path, 'w') as f:
         json.dump(fresh_access_token, f, indent=4)
+
 
 def main():
     access_token_file_path = './data/access_token.json'
     refresh_token = get_refresh_token(access_token_file_path)
     fresh_access_token = get_fresh_access_token(refresh_token)
     save_fresh_access_token(fresh_access_token, access_token_file_path)
-    
+
+
 if __name__ == '__main__':
     main()
