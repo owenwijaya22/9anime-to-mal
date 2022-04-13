e1160a7200dd01b7dda641858af16e2e0e6d0b69
Owen Valentinus <owenwijaya89@gmail.com>
2022-04-13 14:50:53 +0700

    Created .gitignore to exclude data files

--- src/use_token.py
+++ src/use_token.py
@@ -4,9 +4,10 @@ import requests
 with open('./data/access_token.json', 'r+') as f:
     access_token = json.load(f)['access_token']
 
-url = 'https://api.myanimelist.net/v2/users/@me/animelist?fields=list_status'
-response = requests.get(url, headers = {
-        'Authorization': f'Bearer {access_token}'
-        }).json()
-with open('./data/@me.json', 'w') as f:
+url = 'https://api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=100'
+headers = {
+    'Authorization': f'Bearer {access_token}'
+}
+response = requests.get(url, headers=headers).json()
+with open('./data/data.json', 'w', encoding='utf-8') as f:
     json.dump(response, f, indent=4)
