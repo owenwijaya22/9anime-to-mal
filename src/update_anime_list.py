dca592b25ddfe8cbf48cffdf378fd088419865cc
Owen Valentinus <owenwijaya89@gmail.com>
2022-04-13 09:42:20 +0700

    add option to delete or update list, and also catch server error

--- src/update_anime_list.py
+++ src/update_anime_list.py
@@ -25,6 +25,16 @@ async def update_list(access_token, anime_id, session, status_list):
     async with session.put(url, data=data, headers=headers) as response:
         status_list.append(response.status)
 
+async def delete_list(access_token, anime_id, session, status_list):
+    url = f'https://api.myanimelist.net/v2/anime/{anime_id}/my_list_status'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+        }
+    data = {
+        'status': 'plan_to_watch'
+    }
+    async with session.delete(url, data=data, headers=headers) as response:
+        status_list.append(response.status)
         
 
 async def main():
@@ -32,10 +42,17 @@ async def main():
     anime_ids = get_anime_ids()
     status_list = []
     async with aiohttp.ClientSession() as session:
-        tasks =  [update_list(access_token, anime_id, session, status_list) for anime_id in anime_ids]
-        r = await asyncio.gather(*tasks)
-        print(status_list)
-        print(len(status_list))
-    
+        try:
+            task = input('delete(d) or update(u) list?: ')
+            if task == 'd':
+                tasks =  [delete_list(access_token, anime_id, session, status_list) for anime_id in anime_ids]
+            elif task == 'u':
+                tasks =  [update_list(access_token, anime_id, session, status_list) for anime_id in anime_ids]
+            r = await asyncio.gather(*tasks)
+            print(status_list)
+            if 500 in status_list:
+                print("Mal's server experienced server error,  please run the program one more time :D")
+        except Exception as e:
+            print(Exception)
 asyncio.get_event_loop().run_until_complete(main())
 print(f'The program took {time.time()-time_now} seconds')
