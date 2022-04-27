7614530f92c31137c050c26cfc827c3cf7cb2e4a
Owen Valentinus <owenwijaya89@gmail.com>
2022-04-27 22:45:55 +0700

    added host flask api

--- src/get_access_token.py
+++ src/get_access_token.py
@@ -2,31 +2,30 @@ import requests
 import secrets
 import json
 import webbrowser
+import pathlib
+
 
 class AccessToken:
+
     def __init__(self):
-        #declared client_id and client_secret made on https://myanimelist.net/apiconfig 
+        #declared client_id and client_secret made on https://myanimelist.net/apiconfig
         self.client_id = '963e88e06d2cc42e822fecaae6e86c22'
         self.client_secret = 'd506643fc1b782b3cb70c5d5168e78aecbe34f0d83669af953000f86ce6899fa'
-
 
     #get code verifier/challenge as a temporary password
     def get_new_code_verifier(self):
         code_verifier = secrets.token_urlsafe(100)
         return code_verifier[:128]
 
-
     #build the url for mal authentication
     def generate_url(self, client_id, code_challenge):
         url = f'https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id={client_id}&state=YOUR_STATE&code_challenge={code_challenge}&code_challenge_method=plain'
         webbrowser.open(url)
 
-
     #get the authorization code made after the user authenticates mal
     def generate_code(self):
         authorization_code = input('Paste the authorization code: ').strip()
         return authorization_code
-
 
     #get the access token to myanimelist's api
     def generate_access_token(self, client_id, client_secret, authorization_code,
@@ -40,19 +39,22 @@ class AccessToken:
             'grant_type': 'authorization_code'
         }
         r = requests.post(url, data=payload)
-        print(r.url)
         r.raise_for_status()
 
         access_token = r.json()
         print('Token generated successfully!')
 
+        #create data directory to put all the json files/ configs
+        p = pathlib.Path('./data')
+        p.mkdir(exist_ok=True)
+
         with open('./data/access_token.json', 'w') as f:
             json.dump(access_token, f, indent=4)
             print('Token saved in "./data/access_token.json"')
-
 
     def main(self):
         code_verifier = code_challenge = self.get_new_code_verifier()
         self.generate_url(self.client_id, code_challenge)
         authorization_code = self.generate_code()
-        self.generate_access_token(self.client_id, self.client_secret, authorization_code,code_verifier)
+        self.generate_access_token(self.client_id, self.client_secret,
+                                   authorization_code, code_verifier)
--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import asyncio
 from get_access_token import AccessToken
 from update_anime_list import UpdateAnimeList
 
-# access_token_bot = AccessToken()
-# access_token_bot.main()
+access_token_bot = AccessToken()
+access_token_bot.main()
 
-update_bot = UpdateAnimeList()
-asyncio.get_event_loop().run_until_complete(update_bot.main())
+# update_bot = UpdateAnimeList()
+# asyncio.get_event_loop().run_until_complete(update_bot.main())
